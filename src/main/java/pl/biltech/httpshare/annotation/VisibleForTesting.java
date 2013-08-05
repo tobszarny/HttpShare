c5a6e7cf4c27c5a2c7511a54c58f8fc16e1ba10f
bilu <pawel.bilewicz@gmail.com>
2013-08-06 00:31:56 +0200

    network util tweak

---
+++ src/main/java/pl/biltech/httpshare/annotation/VisibleForTesting.java
@@ -0,0 +1,11 @@
+package pl.biltech.httpshare.annotation;
+
+/**
+ * Annotation should be used to emphasise that method visibility restriction is
+ * relaxed for test purpose only. It is better solution than comments
+ * 
+ * @author bilu
+ * 
+ */
+public @interface VisibleForTesting {
+}
--- src/main/java/pl/biltech/httpshare/view/util/NetworkUtil.java
+++ src/main/java/pl/biltech/httpshare/view/util/NetworkUtil.java
@@ -1,5 +1,7 @@
 package pl.biltech.httpshare.view.util;
 
+import static pl.biltech.httpshare.util.Assert.isTrue;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
@@ -7,37 +9,49 @@ import java.net.UnknownHostException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pl.biltech.httpshare.annotation.VisibleForTesting;
+
 /**
- * @author tomek
+ * @author tomek, bilu
  * 
  */
 public class NetworkUtil {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(NetworkUtil.class);
-
-	public static int findFirstFreePort(int startFrom) {
-		logger.debug("findFirstFreePort called");
-		ServerSocket socket = null;
-		try {
-			socket = new ServerSocket(startFrom);
-			logger.info("Found free port " + startFrom);
-			return startFrom;
-		} catch (IOException e) {
-
-		} finally {
-			if (socket != null)
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		}
-		return findFirstFreePort(startFrom + 1);
-	}
+	private static final Logger logger = LoggerFactory.getLogger(NetworkUtil.class);
+	private ServerSocket socket;
 
 	public static String getLocalHostName() throws UnknownHostException {
 		return java.net.InetAddress.getLocalHost().getHostName();
 	}
 
+	public int findFirstFreePort(int startFrom) {
+		isTrue(startFrom > 0);
+		isTrue(startFrom < 65535);
+
+		logger.debug("findFirstFreePort called for: {}", startFrom);
+		try {
+			socket = getServerSocket(startFrom);
+			logger.info("Found free port: {}", startFrom);
+			return startFrom;
+		} catch (IOException e) {
+		} finally {
+			closePort();
+		}
+		return findFirstFreePort(startFrom + 1);
+	}
+
+	private void closePort() {
+		if (socket != null) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				logger.error("Error during socket closing", e);
+			}
+		}
+	}
+
+	@VisibleForTesting
+	ServerSocket getServerSocket(int startFrom) throws IOException {
+		return new ServerSocket(startFrom);
+	}
 }
